699a85a54701f43b201d9fb5c4d2ce767d0c1629
Tommy Ohrling <tommy.ohrling@bnearit.se>
2021-03-29 08:15:52 +0200

    Added some more tests and boundaries.

--- src/main/java/com/ohrlings/arrowheadadapter/AdapterController.java
+++ src/main/java/com/ohrlings/arrowheadadapter/AdapterController.java
@@ -3,19 +3,18 @@ package com.ohrlings.arrowheadadapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/adapter")
+@RestController
+@RequestMapping("/adapter")
 public class AdapterController implements Controller {
 
     Logger logger = LoggerFactory.getLogger(AdapterController.class);
 
     @Override
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> pushData(String testData) {
+    @PostMapping
+    public ResponseEntity<String> pushData(@RequestBody String testData) {
         logger.info(testData);
         return new ResponseEntity<>(testData, HttpStatus.OK);
     }
